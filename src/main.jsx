e13b7f5ea07eeef5402801dccf36b590f4dea256
valeriaAnabeldePrado <anabeltayu@gmail.com>
2023-02-16 11:42:02 -0300

    pageTop

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,12 @@ import './config/i18next-config'
 import App from "./App";
 import { HashRouter as Router, Route } from "react-router-dom";
 import "./index.css";
+import { ScrollToTop } from "./components/Main/scrollTop";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
+      <ScrollToTop/>
       <App />
     </Router>
   </React.StrictMode>
